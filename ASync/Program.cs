cd8b4bd7c19fde3b9d9a1bb7e97ea72b81e7c675
Joseph Eaton <josepheaton04@outlook.com>
2024-12-02 11:10:24 +0000

    Begin completing asynchronous error handling task

--- ASync/Program.cs
+++ ASync/Program.cs
@@ -6,22 +6,50 @@ internal class Program
     {
         async Task SayHelloAndWorld()
         {
+            var _cancelTasks = new CancellationTokenSource();
+
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
             var sayHello = Task.Run(async () =>
             {
-                await Task.Delay(3000);
+                await Task.Delay(new Random().Next(1000, 10001));
                 return "Hello";
-            });
+            },
+            _cancelTasks.Token);
 
             var sayWorld = Task.Run(async () =>
             {
-                await Task.Delay(3000);
+                await Task.Delay(new Random().Next(1000, 10001));
                 return "World";
-            });
+            },
+            _cancelTasks.Token);
 
-            string[] results = await Task.WhenAll([sayHello, sayWorld]);
+            
+            
+            try
+            {
+                string[] results = await Task.WhenAll([sayHello, sayWorld]);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            _cancelTasks.CancelAfter(5000);
+
+            /*private void DoStuff()
+            {
+                _cancelTasks = new CancellationTokenSource();
+
+                var task = new Task(() => { *//* your actions here *//* }, _cancelTasks.Token);
+                task.Start();
+
+                if (!task.Wait(5000)) _cancelTasks.Cancel();
+            }*/
+            
+
+            
 
             stopwatch.Stop();
 
