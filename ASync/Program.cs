37a6bd078fcd969e3f188502d6860b8a3c929b87
digydigdogdead <carlyle.laurent@gmail.com>
2024-12-02 11:24:33 +0100

    Make an async HelloWorld task in program.cs

--- ASync/Program.cs
+++ ASync/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+async Task SayHello()
+{
+    await Task.Delay(2000);
+    Console.WriteLine("Hello, World!");
+}
+
+await SayHello();
